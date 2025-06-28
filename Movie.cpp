7ec6a388aad491f354b61f952a934316e134a8db
Joel-Amroodt <joelamroodt@gmail.com>
2025-06-28 08:59:36 -0500

    Added JSON Parsing

--- Movie.cpp
+++ Movie.cpp
@@ -9,13 +9,13 @@
 #include "OMDbClient.h"
 #include <nlohmann/json.hpp>
 
-#include "OMDbClient.h"
 using json = nlohmann::json;
 
+json j;
 
 Movie::Movie()
     : title(" "), year(" "), genre(" "), release(" "), ratings(" "), f_ratings(" "),
-      top_ten_list(""), comments("")
+      top_ten_list(""), comments(""), poster("")
 
 {
     int rating = fetchRating();
@@ -23,13 +23,49 @@ Movie::Movie()
 
 int Movie::fetchRating()
 {
+    //return client.FetchMovieRatings();
     return client.FetchMovieRatings();
 }
 std::string Movie::getTitle()
 {
-    std::cout << "\nTitle should go here" << std::endl;
-    return title;
+    std::string rawJson = client.get_jsonBody();
+    json j = json::parse(rawJson);
+    title = j.at("Title").get_to(rawJson);
 
+    return title;
 }
+std::string Movie::getYear()
+{
+    std::string rawJson = client.get_jsonBody();
+    json j = json::parse(rawJson);
+    year = j.at("Year").get_to(rawJson);
+
+    return year;
+}
+std::string Movie::getRelease()
+{
+    std::string rawJson = client.get_jsonBody();
+    json j = json::parse(rawJson);
+    release = j.at("Released").get_to(rawJson);
+
+    return release;
+}
+std::string Movie::getGenre()
+{
+    std::string rawJson = client.get_jsonBody();
+    json j = json::parse(rawJson);
+    genre = j.at("Genre").get_to(rawJson);
+
+    return genre;
+}
+std::string Movie::getPoster()
+{
+    std::string rawJson = client.get_jsonBody();
+    json j = json::parse(rawJson);
+    poster = j.at("Poster").get_to(rawJson);
+
+    return poster;
+}
+
 
 
--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 #include "Movie.h"
 #include "OMDbClient.h"
+#include <iostream>
 
 int main()
 {
     Movie movie;
-    movie.getTitle();
+    std::cout << "" << movie.getTitle();
+    std::cout << "\n" << movie.getYear();
+    std::cout << "\n" << movie.getRelease();
+    std::cout << "\n" << movie.getGenre();
+    std::cout << "\n" << movie.getPoster();
+
     // OMDbClient client;
     // return client.FetchMovieRatings();
 
